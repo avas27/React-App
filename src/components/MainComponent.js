d2345e00e77e039090fd08e3932e8e669c2d6b5d
avas27 <abhishekvermasn27@gmail.com>
2019-09-05 22:51:08 +0530

    Single Page Application part-1

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -1,7 +1,11 @@
 import React, { Component } from "react";
 import { DISHES } from "./shared/dishes";
+import { COMMENTS } from "./shared/comments";
+import { PROMOTIONS } from "./shared/promotions";
+import { LEADERS } from "./shared/leaders";
 import "bootstrap/dist/css/bootstrap.min.css";
 import Menu from "./MenuComponent";
+import Contact from "./ContactComponent";
 import DishDetail from "./DishdetailComponent";
 import Header from "./HeaderComponent";
 import Footer from "./FooterComponent";
@@ -12,19 +16,29 @@ class Main extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      dishes: DISHES
+      dishes: DISHES,
+      comments: COMMENTS,
+      promotions: PROMOTIONS,
+      leaders: LEADERS
     };
   }
 
   render() {
     const HomePage = () => {
-      return <Home></Home>;
+      return (
+        <Home
+          dish={this.state.dishes.filter(dish => dish.featured)[0]}
+          promotion={this.state.promotions.filter(promo => promo.featured)[0]}
+          leader={this.state.leaders.filter(leader => leader.featured)[0]}
+        ></Home>
+      );
     };
     return (
       <div>
         <Header />
         <Switch>
           <Route path="/home" component={HomePage}></Route>
+          <Route exact path="/contactus" component={Contact}></Route>
           <Route
             exact
             path="/menu"
